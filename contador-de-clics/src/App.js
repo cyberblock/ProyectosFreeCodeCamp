996f99a2cbda333187ca4539c3ea01a2c0edf197
CyberBlock <54968448+cyberblock@users.noreply.github.com>
2022-07-04 11:17:16 +0200

    up proyect contador
    Subiendo cambios de contador

--- contador-de-clics/src/App.js
+++ contador-de-clics/src/App.js
@@ -1,10 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import freeCodeCampLogo from './imagenes/freecode.png';
 
 function App() {
+
+  // arrow function
+  const manejarClic = () =>{
+    console.log("Clic");
+  }
+
+  const reiniciarContador = () =>{
+    console.log("Reiniciar");
+  }
+
   return (
     <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+            className='freecodecamp-logo'
+            src={freeCodeCampLogo}
+            alt='Logo de freeCodeCamp'
+        />
+      </div>
 
+      <div className='contenedor-principal'>
+        <Contador
+          numClics={5}
+        />
+          <Boton
+            texto='Clic'
+            esBotonDeClic={true}
+            manejarClic={manejarClic}
+          />
+          <Boton
+            texto='Reiniciar'
+            esBotonDeClic={false}
+            manejarClic={reiniciarContador}
+          />
+      </div>
     </div>
   );
 }
---
+++ contador-de-clics/src/componentes/Contador.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style/Contador.css'
+
+
+
+function Contador({numClics}) {
+    return (
+        <div 
+            className='contador'
+        >
+                {numClics}
+        </div>
+    )
+}
+
+
+export default Contador;
