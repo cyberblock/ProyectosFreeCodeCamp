cf1e2ec27fb7c5028495d1ba31adae8f36801ad8
CyberBlock <54968448+cyberblock@users.noreply.github.com>
2022-09-24 17:39:48 +0200

    cs
    cm

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton'
+import freeCodeCampLogo from './imagenes/FreeCodeCampLogo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+        src={freeCodeCampLogo}
+        className='freecodecamp-logo'
+        alt='LogoDeFreeCodeCamp' />
+      </div>
+
+      <div className='contenedor-calculadora'>
+          <div className='fila'>
+            <Boton>1</Boton>
+          </div>
+          <div className='fila'>
+            <Boton>+</Boton>
+          </div>
+          <div className='fila'>
+            <Boton>3</Boton>
+          </div>
+          <div className='fila'>
+            <Boton>4</Boton>
+          </div>
+          <div className='fila'>
+            <Boton>+</Boton>
+          </div>
+      </div>
     </div>
   );
 }
